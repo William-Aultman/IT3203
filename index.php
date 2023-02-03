7a98d0347c0907eea319902baea1cdc8d4c3fdfb
William-Aultman <119095660+William-Aultman@users.noreply.github.com>
2023-02-03 17:39:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,75 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Raven</title>
+    <style>
+        body{
+            background-image: radial-gradient(circle, burlywood, #b78d1c);
+            font-family: Tahoma;
+        }
+        h1{
+            text-align: center;
+        }
+        h2{
+            text-align: center;
+        }
+        h3{
+            text-align: center;
+            border-bottom: 2px solid white;
+            border-top: 2px solid white;
+        }
+        table {
+            width: 100%;
+            border-top: 2px solid white;
+        }
+        th, td{
+            width: 25%;
+            border: 2px solid white;
+            text-align: center;
+        }
+        div{
+            border: 2px solid white;
+        }
+
+    </style>
+</head>
 <body>
-    <h1>Hello, world!</h1>
+<h1>
+    <img src="https://github.com/William-Aultman/images/blob/main/Artboard%201%404x.png" alt="The Raven" width="300" height="300">
+</h1>
+<div>
+    <h2>
+        The Café
+    </h2>
+    <table>
+        <h3>
+            Specialty Beverages
+        </h3>
+        <tr>
+            <th>Cask of Amontillado</th>
+            <th>Nevermore</th>
+            <th>The Beating Heart</th>
+        </tr>
+        <tr>
+            <td>A blonde roast coffee mixed with Amontillado</td>
+            <td>A dark roast coffee with a nutty flavor</td>
+            <td>A very sweet strawberry tea</td>
+        </tr>
+    </table>
+</div>
+<h2>
+    Poetry Slam
+</h2>
+<p>
+    During the later hours of operation, we have a poetry slam where anyone is invited to read their heart aloud.
+</p>
+<h2>
+    Hours and Location
+</h2>
+<p>
+    The hours of operation are from 6AM to 11PM. The café is located on the square of downtown Marietta, GA.
+</p>
+
 </body>
 </html>
